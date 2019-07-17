5ff97ef12a2c530a20ea27fa9ab0521f92735ae9
liasmith <lia.ariel.smith@gmail.com>
2019-07-17 13:21:45 -0500

    New comment added

--- MyFirstProject1/src/module-info.java
+++ MyFirstProject1/src/module-info.java
@@ -2,6 +2,7 @@ module MyFirstProject1 {
 	public static void main(String[] args) {
 		// Output Hello World to the user
 		// This is the new branch
+		// Push change to Git
 		System.out.println("Hello World");
 	}
 }
